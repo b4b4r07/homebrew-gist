391cbce61acc2314354646e2f3a5682c0a89df51
b4b4r07 <b4b4r07@gmail.com>
2017-06-20 22:03:29 +0900

    Update sha256 and links

--- gist.rb
+++ gist.rb
@@ -3,11 +3,11 @@ class Gist < Formula
   version '0.1.5'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/b4b4r07/gist/releases/download/0.1.5/darwin_amd64_gist'
-    sha256 'bf3a57b076ca79c16a6ba1838f4633521292fd6531ce29b3bf87f77baff33470'
+    url 'https://github.com/b4b4r07/gist/releases/download/0.1.5/gist-0.1.5-darwin-amd64.tar.gz'
+    sha256 'ac1c67781b5283aba2694584da32dafdbed2bec33fdafc7c42bfd16481821fc6'
   else
-    url 'https://github.com/b4b4r07/gist/releases/download/0.1.5/darwin_amd64_gist'
-    sha256 'bf3a57b076ca79c16a6ba1838f4633521292fd6531ce29b3bf87f77baff33470'
+    url 'https://github.com/b4b4r07/gist/releases/download/0.1.5/gist-0.1.5-darwin-386.tar.gz'
+    sha256 '793d0e7df8790c64ea9f77e0ae8c7b5f09c2680edd02b56c95215d96c7b43379'
   end
 
   head do
@@ -23,7 +23,7 @@ class Gist < Formula
       mkdir_p buildpath/'.go/src/github.com/b4b4r07'
       ln_s buildpath, buildpath/'.go/src/github.com/b4b4r07/gist'
       system 'make', 'build'
-      bin.install 'build/gist'
+      bin.install 'bin/gist'
     else
       bin.install 'gist'
     end
